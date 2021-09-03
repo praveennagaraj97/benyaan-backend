2bd8a290ad38a4c1cac37e31008366980aa10923
praveenNagaraj97 <praveenacchusn123@gmail.com>
2021-09-03 18:32:18 +0530

    api update

--- admin/src/components/LeftMenu/LeftMenuHeader/index.js
+++ admin/src/components/LeftMenu/LeftMenuHeader/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-import Wrapper from './Wrapper';
+import React from "react";
+import { Link } from "react-router-dom";
+import Wrapper from "./Wrapper";
 
 const LeftMenuHeader = () => (
   <Wrapper>
     <Link to="/" className="leftMenuHeaderLink">
-      <span className="projectName" />
+      <h1
+        style={{
+          color: "white",
+          paddingTop: "14px",
+          fontStyle: "italic",
+          fontSize: "31px",
+        }}
+      >
+        Benyaan
+      </h1>
     </Link>
   </Wrapper>
 );
